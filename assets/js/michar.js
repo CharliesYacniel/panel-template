611df730001f7f69af42b683e3d6589e3f13b5cb
Charlies Yacniel <charliesyacniel@gmail.com>
2022-03-10 01:30:12 -0600

    Charlies|10032022|agregue mas detalles de css y agrueue un char

---
+++ assets/js/michar.js
@@ -0,0 +1,31 @@
+
+const data = {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow',
+      'Grey',
+      'Blue'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [11, 16, 7, 34, 14],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  };
+
+  const config = {
+    type: 'polarArea',
+    data: data,
+    options: {}
+  };
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
